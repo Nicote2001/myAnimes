506ef9cdcaaee7991dd9d019ee79fb140dae738e
Ncote01 <Todcool2001!>
2022-05-20 15:04:29 -0400

    ajout d'un barre de recherche fonctionnelle pour la plus part des animes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
+import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FilterAnimeApiCallerService } from '../ApiCallerService/filterAnime.api-caller.service';
+import { IAnime } from '../objects/anime.model';
+import { CommonService } from '../Shared/common.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,25 +13,53 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   searchFilter:string;
+  lastRequest:Date;
+  animesResults: IAnime[] = [];
 
-  constructor() { }
+
+  constructor(private apiSearch: FilterAnimeApiCallerService, private commonService: CommonService, private router: Router) { }
 
   ngOnInit(): void {
     this.searchFilter = "";
+    this.lastRequest = new Date();
   }
 
   onKeyPress(search:any)
   {   
-      console.log("key", search.key);
-      if(search.key === 'Delete' || search.key === 'Backspace')
+      if(search.data === null)
       {
           this.searchFilter = this.searchFilter.slice(0,-1);
       }
       else
       {
-          this.searchFilter+=search.key;
+          this.searchFilter+=search.data;
       }
       console.log(this.searchFilter);
+
+      this.getSearchBarAnimes();
+
+  }
+
+  getSearchBarAnimes()
+  {
+    this.apiSearch.getSearchBarAnime(this.searchFilter).subscribe(data =>{
+      this.animesResults = data.data.slice(0,5);
+      console.log(data);
+      if(this.searchFilter ==="")
+    {
+      this.animesResults = [];
+    }
+    })
+  }
+
+  goToAnime(anime:string)
+  {
+    var formatedAnimeTitle = this.commonService.FormatAnimeTitle(anime)
+
+    this.animesResults = [];
+    this.searchFilter ="";
+
+    this.router.navigateByUrl('anime/'+formatedAnimeTitle+'/'+1);
   }
 
 }
